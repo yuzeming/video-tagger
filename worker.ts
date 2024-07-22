10143e0c88b634edada672da7e7697881e785385
Zeming Yu <yuzeming@gmail.com>
2024-07-21 18:33:57 -0700

    .

--- worker.ts
+++ worker.ts
@@ -1,100 +1,21 @@
 importScripts("demuxer_mp4.js");
 
+const ThumbnailHeight = 100;
+const ThumbnailWidth = 160;
 
-class WebGLRenderer {
-  #canvas: OffscreenCanvas = null;
-  #ctx: WebGL2RenderingContext = null;
+class Canvas2DRenderer {
+  #canvas: HTMLCanvasElement = null;
+  #ctx: CanvasRenderingContext2D = null;
 
-  static vertexShaderSource = `
-    attribute vec2 xy;
-
-    varying highp vec2 uv;
-
-    void main(void) {
-      gl_Position = vec4(xy, 0.0, 1.0);
-      // Map vertex coordinates (-1 to +1) to UV coordinates (0 to 1).
-      // UV coordinates are Y-flipped relative to vertex coordinates.
-      uv = vec2((1.0 + xy.x) / 2.0, (1.0 - xy.y) / 2.0);
-    }
-  `;
-
-  static fragmentShaderSource = `
-    varying highp vec2 uv;
-
-    uniform sampler2D texture;
-
-    void main(void) {
-      gl_FragColor = texture2D(texture, uv);
-    }
-  `;
-
-  constructor(canvas: OffscreenCanvas) {
+  constructor(canvas: HTMLCanvasElement) {
     this.#canvas = canvas;
-    const gl = this.#ctx = canvas.getContext("webgl2");
-
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, WebGLRenderer.vertexShaderSource);
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-      throw gl.getShaderInfoLog(vertexShader);
-    }
-
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, WebGLRenderer.fragmentShaderSource);
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-      throw gl.getShaderInfoLog(fragmentShader);
-    }
-
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram (shaderProgram );
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      throw gl.getProgramInfoLog(shaderProgram);
-    }
-    gl.useProgram(shaderProgram);
-
-    // Vertex coordinates, clockwise from bottom-left.
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-      -1.0, -1.0,
-      -1.0, +1.0,
-      +1.0, +1.0,
-      +1.0, -1.0
-    ]), gl.STATIC_DRAW);
-
-    const xyLocation = gl.getAttribLocation(shaderProgram, "xy");
-    gl.vertexAttribPointer(xyLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(xyLocation);
-
-    // Create one texture to upload frames to.
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    this.#ctx = canvas.getContext("2d");
   }
 
-  draw(frame) {
+  draw(frame: VideoFrame) {
     this.#canvas.width = frame.displayWidth;
     this.#canvas.height = frame.displayHeight;
-
-    const gl = this.#ctx;
-  
-    // Upload the frame.
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, frame);
-    frame.close();
-
-    // Configure and clear the drawing area.
-    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Draw the frame.
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    this.#ctx.drawImage(frame, 0, 0, frame.displayWidth, frame.displayHeight);
   }
 };
 
@@ -116,13 +37,13 @@ function statusAnimationFrame() {
 }
 
 // Rendering. Drawing is limited to once per animation frame.
-let renderer: WebGLRenderer = null;
+let renderer: Canvas2DRenderer = null;
 let pendingFrame = null;
 let startTime = null;
 let frameCount = 0;
 let currentTime = -1;
 
-function renderFrame(frame) {
+function renderFrame(frame, currentTime) {
   if (!pendingFrame) {
     // Schedule rendering in the next animation frame.
     requestAnimationFrame(renderAnimationFrame);
@@ -140,8 +61,8 @@ function renderAnimationFrame() {
 }
 
 // Startup.
-function start({dataUri, canvas}) {
-  renderer = new WebGLRenderer(canvas);
+async function start({dataUri, canvas}) {
+  renderer = new Canvas2DRenderer(canvas);
 
   // Set up a VideoDecoder.
   const decoder = new VideoDecoder({
@@ -161,7 +82,7 @@ function start({dataUri, canvas}) {
       currentTime = Math.floor(frame.timestamp/1e6);
       setStatus("currentTime", currentTime);
       // Schedule the frame to be rendered.
-      renderFrame(frame);
+      renderFrame(frame, currentTime);
     },
     error(e) {
       setStatus("decode", e);
@@ -182,4 +103,4 @@ function start({dataUri, canvas}) {
 }
 
 // Listen for the start request.
-self.addEventListener("message", message => start(message.data), {once: true});
+self.addEventListener("message", message =>start(message.data), {once: true});
